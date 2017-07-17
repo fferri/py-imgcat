a02d91e075aae74fa20feee7f10a8670de2dda6d
Kaolin Fire <kaolin@erif.org>
2017-07-17 12:26:25 -0700

    added minimal setup script for pip installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='py-imgcat',
+        version='0.1',
+        description='python integration with iTerm2+ imgcat',
+        url='https://github.com/fferri/py-imgcat',
+        author='Federico Ferri',
+        license='BSD-2-Clause',
+        packages=['py-imgcat'],
+        zip_safe=False)
